76868a4a207b655ec9fe5c4451d4a2e41bde5eb4
Acacio S. Costa <acaciosantossi@gmail.com>
2022-05-11 19:34:33 -0300

    choose database config

--- api/src/config/dbConnection.js
+++ api/src/config/dbConnection.js
@@ -7,10 +7,12 @@ const {
     database
 } = require('./env.json')['database']
 
+let connectionString = process.env.NODE_ENV === 'development'
+    ? `postgres://${user}:${password}@${host}:${port}/${database}`
+    : process.env.DATABASE_URL
+
 const getConnection = () => {
-    const pool = new Pool({
-        connectionString: `postgres://${user}:${password}@${host}:${port}/${database}`
-    })
+    const pool = new Pool({ connectionString })
 
     return pool.connect()
 }
